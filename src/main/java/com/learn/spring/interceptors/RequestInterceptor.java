a236b82b95b221c00395ecc77b40dd13003f16cf
Kushal PC <kushalkhandelwal27@gmail.com>
2024-01-07 12:22:03 +0530

    Interceptor - pre, post, after handlers

---
+++ src/main/java/com/learn/spring/interceptors/RequestInterceptor.java
@@ -0,0 +1,33 @@
+package com.learn.spring.interceptors;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.ModelAndView;
+
+@Slf4j
+@Component
+public class RequestInterceptor implements HandlerInterceptor {
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+        log.info("Inside PreHandle");
+        // We can authenticate user here
+        // and send 401 from here
+//        response.sendError(401, "You are not authorized");
+        return false;
+    }
+
+    @Override
+    public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler, ModelAndView modelAndView) throws Exception {
+        log.info("Inside Post Handeler");
+
+    }
+
+    @Override
+    public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex) throws Exception {
+        log.info("Inside After Completion");
+
+    }
+}
